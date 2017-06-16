5675767190e4174ca8a338ed08f3354da2403475
Tsatsan <jordicostafigueras@gmail.com>
2017-06-16 07:42:11 +0200

    last version

--- public/js/controllers/LogoutController.js
+++
@@ -1,8 +0,0 @@
-angular.module('taskAgend')
-  .controller('LogoutController', function ($scope, $location, AuthService, toastr) {
-
-    AuthService.logout()
-    toastr.success('Logged Out successfully', 'Log Out')
-    $location.path('/')
-
-})
--- public/js/services/DataService.js
+++
@@ -1,9 +0,0 @@
-angular.module('taskAgend')
-	.factory('DataService', function ($http) {
-  	function getSecretData () {
-  		return $http.get('/data')
-  			.then(response => response.data)
-  			.catch(() => 'error!!')
-  	}
-  return { getSecretData }
-})
